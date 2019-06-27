4ec97bdf40b07a11331b6e788c94df08b77976b1
Bruno Campos <bcneves13@gmail.com>
2019-06-27 15:14:20 -0300

    landscapeoption

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const airLogger = require('airfluencers-logger');
 const directoryPath = path.join('teste_bruno');
 const forbidenFiles = ['.DS_Store'];
 let log = new airLogger();
+
 const saveImage = (name, buffer) => {
 	fs.createWriteStream(name).write(buffer);
 }
@@ -13,20 +14,38 @@ const treatImage = (folder, file) => {
 	console.log("entrou", `${folder}/${file}`,);
 	let image = sharp(`${folder}/${file}`);
 	image.metadata().then(function(info) {
-		if(info.width > info.height){
-			image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
-				if (err) {
-					console.log(err);
-				}
-				console.log(info);
-			});
+		if (process.env.LANDSCAPE) {
+			if(info.width < info.height){
+				image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
+					if (err) {
+						console.log(err);
+					}
+					console.log(info);
+				});
+			} else {
+				image.toFile(`./resolved-imgs/${file}`, (err, info) => {
+					if (err) {
+						console.log(err);
+					}
+					console.log(info);
+				})
+			}
 		} else {
-			image.toFile(`./resolved-imgs/${file}`, (err, info) => {
-				if (err) {
-					console.log(err);
-				}
-				console.log(info);
-			})
+			if(info.width > info.height){
+				image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
+					if (err) {
+						console.log(err);
+					}
+					console.log(info);
+				});
+			} else {
+				image.toFile(`./resolved-imgs/${file}`, (err, info) => {
+					if (err) {
+						console.log(err);
+					}
+					console.log(info);
+				})
+			}
 		}
 	}).then(function(data) {
 		// data contains a WebP image half the width and height of the original JPEG
