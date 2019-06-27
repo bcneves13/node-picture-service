e04d8a70b3a8f5b1fde68062e62cd8e9e8096b76
Bruno Campos <bcneves13@gmail.com>
2019-06-27 15:24:48 -0300

    improve if and remove coments

--- app.js
+++ app.js
@@ -14,55 +14,25 @@ const treatImage = (folder, file) => {
 	console.log("entrou", `${folder}/${file}`,);
 	let image = sharp(`${folder}/${file}`);
 	image.metadata().then(function(info) {
-		if (process.env.LANDSCAPE) {
-			if(info.width < info.height){
-				image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
-					if (err) {
-						console.log(err);
-					}
-					console.log(info);
-				});
-			} else {
-				image.toFile(`./resolved-imgs/${file}`, (err, info) => {
-					if (err) {
-						console.log(err);
-					}
-					console.log(info);
-				})
-			}
+		const condition = process.env.LANDSCAPE ? info.width < info.height : info.width > info.height;
+		if(condition){
+			image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
+				if (err) {
+					console.log(err);
+				}
+				console.log(info);
+			});
 		} else {
-			if(info.width > info.height){
-				image.rotate(90).toFile(`./resolved-imgs/${file}`, (err, info) => {
-					if (err) {
-						console.log(err);
-					}
-					console.log(info);
-				});
-			} else {
-				image.toFile(`./resolved-imgs/${file}`, (err, info) => {
-					if (err) {
-						console.log(err);
-					}
-					console.log(info);
-				})
-			}
+			image.toFile(`./resolved-imgs/${file}`, (err, info) => {
+				if (err) {
+					console.log(err);
+				}
+				console.log(info);
+			})
 		}
 	}).then(function(data) {
 		// data contains a WebP image half the width and height of the original JPEG
 	});
-	// console.log(sharp('./imgs/test.jpg'));
-	// sharp(`${folder}/${file}`).rotate(90).toBuffer((err, outputBuffer, info) => {
-	// 	if(err){
-	// 		log.err(err);
-	// 		return false;
-	// 	}
-	// 	log.info(outputBuffer, info);
-	// 	if(info.width > info.height){
-	// 		saveImage(`./resolved-imgs/${file}`, outputBuffer);
-	// 	} else {
-
-	// 	}
-	// });
 }
 
 fs.readdir(directoryPath, function (err, files) {
